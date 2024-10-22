52b2569ded1905f3ddffcbdfa64f3a4ed7e536fa
Jabriel <72719356+JabrielHo@users.noreply.github.com>
2024-10-22 20:51:45 +0800

    Fix AnonLogin userId value

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -9,7 +9,7 @@ class UserService {
         const userId = auth.currentUser.uid
         const userDocRef = doc(db, 'users', userId)
         await setDoc(userDocRef, {
-          userId: 'userId',
+          userId: userId,
           userName: 'TestUser',
           profilePicture:
             'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
